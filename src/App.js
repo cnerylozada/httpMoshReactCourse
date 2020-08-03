3d66dbd22de1d8b98977771812de8146b583a23b
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-08-03 00:11:17 -0500

    feat: initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const handleAdd = () => {
+    console.log("Add");
+  };
+
+  const handleUpdate = (post) => {
+    console.log("Update", post);
+  };
+
+  const handleDelete = (post) => {
+    console.log("Delete", post);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button className="btn btn-primary" onClick={handleAdd}>
+        Add
+      </button>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.title}</td>
+              <td>
+                <button
+                  className="btn btn-info btn-sm"
+                  onClick={() => handleUpdate(post)}
+                >
+                  Update
+                </button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleDelete(post)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 }
 
